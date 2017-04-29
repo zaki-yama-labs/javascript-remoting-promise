94019395b6bb33cd04edee9c5bc5b3c00909ad1e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-04-30 05:22:20 +0900

    tweak

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
 import { util } from 'react-lightning-design-system';
 
+import configureStore from './store/configureStore';
+
 import App from './containers/App';
-import '../stylesheets/index.scss';
 
 const store = configureStore();
 
-export const init = function(el, assetRoot) {
+export const init = function (el, assetRoot) {
   util.setAssetRoot(assetRoot);
   console.log('Set asset root as', util.getAssetRoot());
 
