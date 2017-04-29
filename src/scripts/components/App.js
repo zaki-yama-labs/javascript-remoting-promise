fa7a2370c50bb6acfc9bbadba561b7741aae099e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-04-30 05:10:04 +0900

    Update Component

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { Button, Form, Input, DateInput, Spinner, Toast, util } from 'react-lightning-design-system';
+import { Button, Form, Spinner, Toast } from 'react-lightning-design-system';
 
 export default class App extends React.Component {
-  // onClick(e) {
-    // this.props.controller.greeting((result, event) => {
-      // console.log('result:', result);
-      // console.log('event:', event);
-    // });
-  // }
-
   render() {
     return (
       <div>
-        {this.props.errorMessage
-          ? <Toast
-              icon="warning"
-              level="error"
-              onClose={this.props.onCloseToast}
-              alertTexture={false}
-            >
-              {this.props.errorMessage}
-            </Toast>
-          : null}
-        <h2 className="slds-m-vertical--medium">Form Inline</h2>
+        {this.props.errorMessage ?
+          <Toast
+            icon="warning"
+            level="error"
+            onClose={this.props.onCloseToast}
+            alertTexture={false}
+          >
+            {this.props.errorMessage}
+          </Toast> : null}
+        <div className="slds-text-heading--large">JavaScript Remoting using Promise Example.</div>
         <Form type="inline">
-          <Input label="Text Field #1" type="text" placeholder="Input text here" />
-          <DateInput label="DateInput #1" defaultValue="2015-12-24" required />
           <Button type="brand" onClick={this.props.onClick}>Submit</Button>
           <Button type="destructive" onClick={this.props.onClickError}>Submit(Error)</Button>
         </Form>
